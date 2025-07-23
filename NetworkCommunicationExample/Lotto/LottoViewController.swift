4d76aa44dd2c1b801051e473e0c6c6f686f404db
rmfls0606 <rmfls0606@naver.com>
2025-07-23 17:14:15 +0900

    당첨 안내 보여주는 View 구성 및 AutoLayout 설정

--- NetworkCommunicationExample/Lotto/LottoViewController.swift
+++ NetworkCommunicationExample/Lotto/LottoViewController.swift
@@ -16,6 +16,27 @@ class LottoViewController: UIViewController {
         return textField
     }()
     
+    private let winningNumberTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "당첨번호 안내"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let drawDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2020-05-30 추첨"
+        label.textColor = .systemGray3
+        label.font = .systemFont(ofSize: 10)
+        return label
+    }()
+    
+    private let winningNumberContainer: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,6 +49,10 @@ class LottoViewController: UIViewController {
 extension LottoViewController: ViewDesignProtocol{
     func configureHierarchy() {
         view.addSubview(lottoRoundTextField)
+        
+        view.addSubview(winningNumberContainer)
+        winningNumberContainer.addSubview(winningNumberTitleLabel)
+        winningNumberContainer.addSubview(drawDateLabel)
     }
 
     func configureLayout() {
@@ -35,6 +60,22 @@ extension LottoViewController: ViewDesignProtocol{
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             make.height.equalTo(44)
+        }
+        
+        winningNumberContainer.snp.makeConstraints { make in
+            make.top.equalTo(lottoRoundTextField.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(36)
+        }
+        
+        winningNumberTitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        drawDateLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.centerY.equalToSuperview()
         }
     }
 
