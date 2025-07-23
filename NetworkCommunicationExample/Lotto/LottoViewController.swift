9e024f6a84976e98dfafc38b51f32016e1cc2258
rmfls0606 <rmfls0606@naver.com>
2025-07-23 16:24:00 +0900

    lotto 화면에 보여줄 TextField UI 구성 및 AutoLayout 설정

--- NetworkCommunicationExample/Lotto/LottoViewController.swift
+++ NetworkCommunicationExample/Lotto/LottoViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import SnapKit
 
 class LottoViewController: UIViewController {
-
+    
+    private let lottoRoundTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .orange
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+}
+
+extension LottoViewController: ViewDesignProtocol{
+    func configureHierarchy() {
+        view.addSubview(lottoRoundTextField)
+    }
+
+    func configureLayout() {
+        lottoRoundTextField.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.height.equalTo(44)
+        }
+    }
+
+    func configureView() {
+        navigationItem.title = "Lotto"
+        
+        view.backgroundColor = .white
     }
 }
