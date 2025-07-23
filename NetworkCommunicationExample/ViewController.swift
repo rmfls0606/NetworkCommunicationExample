a98fb9b84c102a44e3f9ced822334c94a0637f3c
rmfls0606 <rmfls0606@naver.com>
2025-07-23 15:35:11 +0900

    - ViewDesignProtocol 채택 - 로또 뷰로 이동할 수 있는 버튼 UI 구성 및 AutoLayout 설정

--- NetworkCommunicationExample/ViewController.swift
+++ NetworkCommunicationExample/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private let lottoMoveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Lotto", for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemGray2.cgColor
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
     }
-
-
 }
 
+extension ViewController: ViewDesignProtocol{
+    func configureHierarchy() {
+        view.addSubview(lottoMoveButton)
+    }
+
+    func configureLayout() {
+        lottoMoveButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func configureView() {
+        view.backgroundColor = .white
+    }
+}
