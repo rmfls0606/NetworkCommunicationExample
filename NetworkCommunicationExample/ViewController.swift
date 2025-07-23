95f916c30d9c026ce744add434155fb97c574229
rmfls0606 <rmfls0606@naver.com>
2025-07-23 15:15:22 +0900

    Initial Commit

---
+++ NetworkCommunicationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkCommunicationExample
+//
+//  Created by 이상민 on 7/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
