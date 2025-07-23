190e5b376763e0d84e481db1f2905170a7e53dc4
rmfls0606 <rmfls0606@naver.com>
2025-07-23 15:25:52 +0900

    뷰 설정 분리용 ViewDesignProtocol 생성

---
+++ NetworkCommunicationExample/ViewDesignProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  NetworkCommunicationExample
+//
+//  Created by 이상민 on 7/23/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol: AnyObject{
+    func configureHierarchy()
+    func configureView()
+    func configureLayout()
+}
