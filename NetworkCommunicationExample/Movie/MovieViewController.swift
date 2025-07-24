2ccd0a9e1a683a82c50da0259edcabeeccbade2e
rmfls0606 <rmfls0606@naver.com>
2025-07-24 16:02:46 +0900

    [Feat] 영화 검색 화면에서 서치바 UI 구성 및 AutoLayout 설정

--- NetworkCommunicationExample/Movie/MovieViewController.swift
+++ NetworkCommunicationExample/Movie/MovieViewController.swift
@@ -6,9 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class MovieViewController: UIViewController {
 
+    private let movieSearchTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.textColor = .systemGray3
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.systemGray3.cgColor
+        return textField
+    }()
+    
+    private let movieSearchButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.attributedTitle = AttributedString("검색", attributes: AttributeContainer([
+            .font: UIFont.systemFont(ofSize: 14)
+        ]))
+        config.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 16,
+            bottom: 0,
+            trailing: 16
+        )
+        config.baseForegroundColor = .black
+        
+        button.configuration = config
+        button.backgroundColor = .systemGray3
+        return button
+    }()
+    
+    private let movieSearchView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +55,47 @@ class MovieViewController: UIViewController {
 
 extension MovieViewController: ViewDesignProtocol{
     func configureHierarchy() {
+        view.addSubview(movieSearchView)
         
+        movieSearchView.addSubview(movieSearchTextField)
+        movieSearchView.addSubview(movieSearchButton)
     }
 
     func configureLayout() {
-
+        movieSearchView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(44)
+        }
+        
+        movieSearchTextField.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview()
+            make.trailing.equalTo(movieSearchButton.snp.leading).offset(-8)
+        }
+        
+        movieSearchTextField
+            .setContentHuggingPriority(.defaultLow, for: .horizontal)
+        movieSearchTextField
+            .setContentCompressionResistancePriority(
+                .defaultLow,
+                for: .horizontal
+            )
+        
+        movieSearchButton.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview()
+        }
+        movieSearchButton
+            .setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        movieSearchButton
+            .setContentCompressionResistancePriority(
+                .defaultHigh,
+                for: .horizontal
+            )
     }
     
     func configureView() {
-        view.backgroundColor = .orange
+        navigationItem.title = "Movie"
+        
+        view.backgroundColor = .black
     }
 }
